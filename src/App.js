e631d217e241edc005587d1c07e6c8954b8b446c
Nwe Nwe Lin <nwenwelinn@gicjp.com>
2024-02-22 11:56:29 +0630

    solve error

--- src/App.js
+++ src/App.js
@@ -7,40 +7,49 @@ function App() {
   const [num1, setNum1] = useState('');
   const [num2, setNum2] = useState('');
   const [add, setAdd] = useState('');
+
+  const handleNum1Change = (event) => {
+    setNum1(event.target.value);
+  };
+
+  const handleNum2Change = (event) => {
+    setNum2(event.target.value);
+  };
+
   const sum = (event) => {
-    event.preventDefault();
-    if(num1 === 0 || num2 === 0){
+     event.preventDefault();
+    if(num1 === '' || num2 === ''){
       alert('Please enter a number');
     }
     else{
       const add = parseInt(num1) + parseInt(num2);
       setAdd(add);
     }
-  }
+  };
   return (
     <div className="App">
       <div className='container'>
           <form onSubmit={sum} className='login-form'>
             <h1>Sum Calculator</h1>
             {/* number one */}
-            <div class="form-input-material">
+            <div className="form-input-material">
               <label htmlFor='num1'>Number 1:</label>
-              <input type='number' name='num1' id='num1' placeholder='Enter a number' className='form-control' required 
-                onChange={(event) => setNum1(event.target.value)}
-              />
+              <input type='number' placeholder='Enter a number' className='form-control' 
+                onChange={handleNum1Change}
+                required/>
             </div>
 
             {/* number two */}
-            <div class="form-input-material">
+            <div className="form-input-material">
               <label htmlFor='num2'>Number 2:</label>
-              <input type='number' name='num2' id='num2' placeholder='Enter a number' className='form-control' required 
-                onChange={(event) => setNum2(event.target.value)}
-              />
+              <input type='number' placeholder='Enter a number' className='form-control' 
+                onChange={handleNum2Change}
+                required/>
             </div>
 
             {/* sum button */}
             <div>
-              <button class="btn btn-primary btn-ghost mt-2" type='submit'>Sum</button>
+              <button className="btn btn-primary mt-2" type='submit'>Sum</button>
             </div>
 
             {/* show result */}
