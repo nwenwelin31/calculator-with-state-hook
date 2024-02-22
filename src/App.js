c137e294593d64aba4699c6f57acfc68d74b8571
Nwe Nwe Lin <nwenwelinn@gicjp.com>
2024-02-22 16:12:04 +0630

    update

--- src/App.js
+++ src/App.js
@@ -1,60 +1,107 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [num1, setNum1] = useState('');
-  const [num2, setNum2] = useState('');
-  const [add, setAdd] = useState('');
+  const [num1, setNum1] = useState('Not');
+  const [num2, setNum2] = useState('Not');
+  const [result, setResult] = useState();
+  const [operation, setOperation] = useState();
+  const [action, setAction] = useState();
 
+  
   const handleNum1Change = (event) => {
     setNum1(event.target.value);
   };
 
-  const handleNum2Change = (event) => {
-    setNum2(event.target.value);
-  };
+    const handleNum2Change = (event) => {
+      setNum2(event.target.value);
+    };
+    const handleOperation = (op) => {
+      setOperation(op);
+    };
 
-  const sum = (event) => {
-     event.preventDefault();
-    if(num1 === '0' || num2 === '0'){
-      alert('Please enter a number');
+    
+const calculator = () => {
+  let result;
+  let action;
+    switch (operation) {
+      case '+':
+        result =(parseInt(num1) + parseInt(num2));
+        action ="Sum Result : ";
+        break;
+      case '-':
+        result = (parseInt(num1) - parseInt(num2));
+        action ="Subtract Result : ";
+        break;
+      case '*':
+        result = (parseInt(num1) * parseInt(num2));
+        action ="Multiply Result : ";
+        break;
+      case '/':
+        // Check if num2 is not zero before performing division
+        if (parseInt(num2) !== 0) {
+          result = (parseInt(num1) / parseInt(num2));
+          action ="Divide Result : ";
+        } else {
+          result = "Cannot divide by zero";
+        }
+        break;
+      default:
+        break;
     }
-    else{
-      const add = parseInt(num1) + parseInt(num2);
-      setAdd(add);
-    }
-  };
+    setResult(result);
+    setAction(action);
+};
+
+useEffect(() => {
+  calculator();
+  console.log('calculating')
+}, [num1,num2,operation]);
+
+
+  // const sum = (event) => {
+  //    event.preventDefault();
+  //   if(num1 === '0' || num2 === '0'){
+  //     alert('Please enter a number');
+  //   }
+  //   else{
+  //     const add = parseInt(num1) + parseInt(num2);
+  //     setAdd(add);
+  //   }
+  // };
   return (
     <div className="App">
       <div className='container'>
-          <form onSubmit={sum} className='login-form'>
-            <h1>Sum Calculator</h1>
+          <form className='login-form'>
+            <h1>Calculator</h1>
             {/* number one */}
             <div className="form-input-material">
-              <label htmlFor='num1'>Number 1:</label>
-              <input type='number' placeholder='Enter a number' className='form-control' 
+              <label htmlFor=''>Number 1:</label>
+              <input type='number' name='num1' placeholder='Enter a number' className='form-control' 
                 onChange={handleNum1Change}
                 required/>
             </div>
 
             {/* number two */}
             <div className="form-input-material">
-              <label htmlFor='num2'>Number 2:</label>
-              <input type='number' placeholder='Enter a number' className='form-control' 
-                onChange={handleNum2Change}
-                required/>
+              <label htmlFor=''>Number 2:</label>
+              <input type='number' name='num2' placeholder='Enter a number' className='form-control' 
+                onChange={handleNum2Change} required/>
             </div>
 
             {/* sum button */}
-            <div>
-              <button className="btn btn-primary mt-2" type='submit'>Sum</button>
+            <div className='d-flex'>
+              <button className="btn btn-primary mx-1" type='button' onClick={() => handleOperation('+')}>+</button>
+              <button className="btn btn-primary mx-1" type='button' onClick={() => handleOperation('-')}>-</button>
+              <button className="btn btn-primary mx-1" type='button' onClick={() => handleOperation('*')}>*</button>
+              <button className="btn btn-primary mx-1" type='button' onClick={() => handleOperation('/')}>/</button>
             </div>
 
             {/* show result */}
             <div>
-              <h3>Sum Result : {add}</h3>
+              <h3>{ isNaN(result) ? '':  action+result}</h3>
             </div>
           </form>
       </div>
